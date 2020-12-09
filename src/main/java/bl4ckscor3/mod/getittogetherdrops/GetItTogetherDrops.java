7937f1c1533fece9cd5e8cce7df06942c3a210b5
Alek Rollyson <alek.rollyson@gmail.com>
2020-12-09 10:22:36 -0500

    Fix server incompatibility messages

--- src/main/java/bl4ckscor3/mod/getittogetherdrops/GetItTogetherDrops.java
+++ src/main/java/bl4ckscor3/mod/getittogetherdrops/GetItTogetherDrops.java
@@ -5,9 +5,11 @@ import org.apache.commons.lang3.tuple.Pair;
 import net.minecraftforge.common.ForgeConfigSpec;
 import net.minecraftforge.common.ForgeConfigSpec.BooleanValue;
 import net.minecraftforge.common.ForgeConfigSpec.DoubleValue;
+import net.minecraftforge.fml.ExtensionPoint;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.network.FMLNetworkConstants;
 
 @Mod(GetItTogetherDrops.MODID)
 public class GetItTogetherDrops
@@ -16,6 +18,9 @@ public class GetItTogetherDrops
 
 	public GetItTogetherDrops()
 	{
+		//Make sure the mod being absent on the other network side does not cause the client to display the server as incompatible
+		ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
+
 		ModLoadingContext.get().registerConfig(ModConfig.Type.SERVER, Configuration.CONFIG_SPEC);
 	}
 
