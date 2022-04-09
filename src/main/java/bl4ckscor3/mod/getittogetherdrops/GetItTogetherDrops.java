9b52168dcefb41dddbece7ca0209d791e9820b59
bl4ckscor3 <bl4ckscor33@gmail.com>
2022-04-09 17:05:17 +0200

    make mod not required on clients

--- src/main/java/bl4ckscor3/mod/getittogetherdrops/GetItTogetherDrops.java
+++ src/main/java/bl4ckscor3/mod/getittogetherdrops/GetItTogetherDrops.java
@@ -9,7 +9,7 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
-@Mod(modid=GetItTogetherDrops.MODID, name="Get It Together, Drops!", version="1.0")
+@Mod(modid=GetItTogetherDrops.MODID, name="Get It Together, Drops!", version="1.0.1", serverSideOnly=true, acceptableRemoteVersions="*")
 @EventBusSubscriber(modid=GetItTogetherDrops.MODID)
 public class GetItTogetherDrops
 {
