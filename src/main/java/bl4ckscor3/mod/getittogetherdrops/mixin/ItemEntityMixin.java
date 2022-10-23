51c4fe947eec78600b27dba7c7dcbfa9fbd53060
bl4ckscor3 <bl4ckscor33@gmail.com>
2022-10-23 15:08:18 +0200

    improve injection point

--- src/main/java/bl4ckscor3/mod/getittogetherdrops/mixin/ItemEntityMixin.java
+++ src/main/java/bl4ckscor3/mod/getittogetherdrops/mixin/ItemEntityMixin.java
@@ -10,22 +10,19 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ItemEntity.class)
 public abstract class ItemEntityMixin {
-	@Inject(method = "mergeWithNeighbours", at = @At("HEAD"), cancellable = true)
+	@Inject(method = "mergeWithNeighbours", at = @At(value = "INVOKE_ASSIGN", id = "Lnet/minecraft/world/entity/item/ItemEntity;isMergable()Z"), cancellable = true)
 	private void mergeWithNeighbours(CallbackInfo info) {
 		ItemEntity me = (ItemEntity) (Object) this;
 		ItemStack item = me.getItem();
 
-		if (item.is(GetItTogetherDrops.IGNORED)) //fall back to vanilla behavior
+		if (item.is(GetItTogetherDrops.IGNORED)) //if part of tag: fall back to vanilla behavior
 			return;
-
-		ItemEntityInvoker invokeMe = (ItemEntityInvoker) me;
-
-		if (invokeMe.callIsMergable() && !item.is(GetItTogetherDrops.DO_NOT_COMBINE)) { //do not combine the item at all
+		else if (!item.is(GetItTogetherDrops.DO_NOT_COMBINE)) { //if part of tag: do not combine the item at all
 			double radius = GetItTogetherDrops.radius();
 			boolean checkY = GetItTogetherDrops.checkY();
 
 			for (ItemEntity ei : me.level.getEntitiesOfClass(ItemEntity.class, me.getBoundingBox().inflate(radius, checkY ? radius : 0.0D, radius), e -> e != me && ((ItemEntityInvoker) e).callIsMergable())) {
-				invokeMe.callTryToMerge(ei);
+				((ItemEntityInvoker) me).callTryToMerge(ei);
 
 				if (me.isRemoved())
 					break;
