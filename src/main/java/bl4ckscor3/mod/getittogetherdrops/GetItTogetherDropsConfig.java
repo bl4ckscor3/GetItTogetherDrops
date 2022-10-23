b88381c7d22a069bb8bd9894f387d2d4926411ea
bl4ckscor3 <bl4ckscor33@gmail.com>
2022-10-23 14:32:43 +0200

    code formatting

--- src/main/java/bl4ckscor3/mod/getittogetherdrops/GetItTogetherDropsConfig.java
+++ src/main/java/bl4ckscor3/mod/getittogetherdrops/GetItTogetherDropsConfig.java
@@ -10,9 +10,8 @@ import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
 import net.minecraftforge.fml.event.config.ModConfigEvent;
 
-@EventBusSubscriber(modid=GetItTogetherDrops.MODID, bus=Bus.MOD)
-public class GetItTogetherDropsConfig
-{
+@EventBusSubscriber(modid = GetItTogetherDrops.MODID, bus = Bus.MOD)
+public class GetItTogetherDropsConfig {
 	public static final ForgeConfigSpec CONFIG_SPEC;
 	public static final GetItTogetherDropsConfig CONFIG;
 	private final DoubleValue radiusConfigValue;
@@ -20,29 +19,21 @@ public class GetItTogetherDropsConfig
 	public static double radius;
 	public static boolean checkY;
 
-	static
-	{
-		Pair<GetItTogetherDropsConfig,ForgeConfigSpec> specPair = new ForgeConfigSpec.Builder().configure(GetItTogetherDropsConfig::new);
+	static {
+		Pair<GetItTogetherDropsConfig, ForgeConfigSpec> specPair = new ForgeConfigSpec.Builder().configure(GetItTogetherDropsConfig::new);
 
 		CONFIG_SPEC = specPair.getRight();
 		CONFIG = specPair.getLeft();
 	}
 
-	public GetItTogetherDropsConfig(ForgeConfigSpec.Builder builder)
-	{
-		radiusConfigValue = builder
-				.comment("The radius (in blocks) that dropped items should check around them for other dropped items to combine with. (\"0.5\" is vanilla behavior)")
-				.defineInRange("radius", 2.0D, 0.5D, 500.0D);
-		checkYConfigValue = builder
-				.comment("Set to true if you want dropped items to also check above and below them, set to false if not (\"false\" is vanilla behavior)")
-				.define("checkY", true);
+	public GetItTogetherDropsConfig(ForgeConfigSpec.Builder builder) {
+		radiusConfigValue = builder.comment("The radius (in blocks) that dropped items should check around them for other dropped items to combine with. (\"0.5\" is vanilla behavior)").defineInRange("radius", 2.0D, 0.5D, 500.0D);
+		checkYConfigValue = builder.comment("Set to true if you want dropped items to also check above and below them, set to false if not (\"false\" is vanilla behavior)").define("checkY", true);
 	}
 
 	@SubscribeEvent
-	public static void onModConfig(ModConfigEvent event)
-	{
-		if(event.getConfig().getSpec() == GetItTogetherDropsConfig.CONFIG_SPEC)
-		{
+	public static void onModConfig(ModConfigEvent event) {
+		if (event.getConfig().getSpec() == GetItTogetherDropsConfig.CONFIG_SPEC) {
 			radius = CONFIG.radiusConfigValue.get();
 			checkY = CONFIG.checkYConfigValue.get();
 		}
--- src/main/java/bl4ckscor3/mod/getittogetherdrops/mixin/ItemEntityMixin.java
+++ src/main/java/bl4ckscor3/mod/getittogetherdrops/mixin/ItemEntityMixin.java
@@ -10,28 +10,23 @@ import bl4ckscor3.mod.getittogetherdrops.GetItTogetherDropsConfig;
 import net.minecraft.world.entity.item.ItemEntity;
 
 @Mixin(ItemEntity.class)
-public class ItemEntityMixin
-{
-	@Inject(method="mergeWithNeighbours", at=@At("HEAD"), cancellable=true)
-	private void mergeWithNeighbours(CallbackInfo info)
-	{
-		ItemEntity me = (ItemEntity)(Object)this;
+public class ItemEntityMixin {
+	@Inject(method = "mergeWithNeighbours", at = @At("HEAD"), cancellable = true)
+	private void mergeWithNeighbours(CallbackInfo info) {
+		ItemEntity me = (ItemEntity) (Object) this;
 
-		if(me.getItem().is(GetItTogetherDrops.IGNORED))
+		if (me.getItem().is(GetItTogetherDrops.IGNORED))
 			return;
-		else if(me.isMergable())
-		{
+		else if (me.isMergable()) {
 			double radius = GetItTogetherDropsConfig.radius;
 			boolean checkY = GetItTogetherDropsConfig.checkY;
 
-			for(ItemEntity ei : me.level.getEntitiesOfClass(ItemEntity.class, me.getBoundingBox().inflate(radius, checkY ? radius : 0.0D, radius), e -> e != me && e.isMergable()))
-			{
+			for (ItemEntity ei : me.level.getEntitiesOfClass(ItemEntity.class, me.getBoundingBox().inflate(radius, checkY ? radius : 0.0D, radius), e -> e != me && e.isMergable())) {
 				me.tryToMerge(ei);
 
-				if(me.isRemoved())
+				if (me.isRemoved())
 					break;
 			}
-
 		}
 
 		info.cancel(); //"return;" in the original method
