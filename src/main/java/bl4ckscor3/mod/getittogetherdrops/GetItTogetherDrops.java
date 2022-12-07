2006db3cf9d415ab855c8b58fcd6a7bc16ea4eee
bl4ckscor3 <bl4ckscor33@gmail.com>
2022-12-07 19:34:50 +0100

    update to 1.19.3

--- src/main/java/bl4ckscor3/mod/getittogetherdrops/GetItTogetherDrops.java
+++ src/main/java/bl4ckscor3/mod/getittogetherdrops/GetItTogetherDrops.java
@@ -3,15 +3,15 @@ package bl4ckscor3.mod.getittogetherdrops;
 import me.shedaniel.autoconfig.AutoConfig;
 import me.shedaniel.autoconfig.serializer.JanksonConfigSerializer;
 import net.fabricmc.api.ModInitializer;
-import net.minecraft.core.Registry;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
 
 public class GetItTogetherDrops implements ModInitializer {
 	public static final String MODID = "getittogetherdrops";
-	public static final TagKey<Item> IGNORED = TagKey.create(Registry.ITEM_REGISTRY, new ResourceLocation(MODID, "ignored"));
-	public static final TagKey<Item> DO_NOT_COMBINE = TagKey.create(Registry.ITEM_REGISTRY, new ResourceLocation(MODID, "do_not_combine"));
+	public static final TagKey<Item> IGNORED = TagKey.create(Registries.ITEM, new ResourceLocation(MODID, "ignored"));
+	public static final TagKey<Item> DO_NOT_COMBINE = TagKey.create(Registries.ITEM, new ResourceLocation(MODID, "do_not_combine"));
 	private static GetItTogetherDropsConfig config;
 
 	@Override
