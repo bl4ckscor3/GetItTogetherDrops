63781aaa76f9306d3946e9183a280d05ca220f0e
bl4ckscor3 <bl4ckscor33@gmail.com>
2025-07-16 15:24:27 +0200

    Support the config screen

---
+++ src/main/java/bl4ckscor3/mod/getittogetherdrops/GetItTogetherDropsClient.java
@@ -0,0 +1,14 @@
+package bl4ckscor3.mod.getittogetherdrops;
+
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.client.gui.ConfigurationScreen;
+import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
+
+@Mod(value = GetItTogetherDrops.MODID, dist = Dist.CLIENT)
+public class GetItTogetherDropsClient {
+	public GetItTogetherDropsClient(ModContainer modContainer) {
+		modContainer.registerExtensionPoint(IConfigScreenFactory.class, ConfigurationScreen::new);
+	}
+}
