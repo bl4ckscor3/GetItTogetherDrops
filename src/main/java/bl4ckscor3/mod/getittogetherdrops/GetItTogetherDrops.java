8384775365922a2d08ef25fad2be08ddb1b30c1b
bl4ckscor3 <bl4ckscor33@gmail.com>
2025-12-10 23:46:57 +0100

    Update to 1.21.11

--- src/main/java/bl4ckscor3/mod/getittogetherdrops/GetItTogetherDrops.java
+++ src/main/java/bl4ckscor3/mod/getittogetherdrops/GetItTogetherDrops.java
@@ -1,6 +1,6 @@
 package bl4ckscor3.mod.getittogetherdrops;
 
-import net.minecraft.resources.ResourceLocation;
+import net.minecraft.resources.Identifier;
 import net.minecraft.tags.ItemTags;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
@@ -11,8 +11,8 @@ import net.neoforged.fml.config.ModConfig;
 @Mod(GetItTogetherDrops.MODID)
 public class GetItTogetherDrops {
 	public static final String MODID = "getittogetherdrops";
-	public static final TagKey<Item> IGNORED = ItemTags.create(ResourceLocation.fromNamespaceAndPath(MODID, "ignored"));
-	public static final TagKey<Item> DO_NOT_COMBINE = ItemTags.create(ResourceLocation.fromNamespaceAndPath(MODID, "do_not_combine"));
+	public static final TagKey<Item> IGNORED = ItemTags.create(Identifier.fromNamespaceAndPath(MODID, "ignored"));
+	public static final TagKey<Item> DO_NOT_COMBINE = ItemTags.create(Identifier.fromNamespaceAndPath(MODID, "do_not_combine"));
 
 	public GetItTogetherDrops(ModContainer modContainer) {
 		modContainer.registerConfig(ModConfig.Type.SERVER, GetItTogetherDropsConfig.CONFIG_SPEC);
